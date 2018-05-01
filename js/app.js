e296bc1896919b55159bab48af012491ff4a94db
Paul Standley <paulstandley72@outlook.com>
2018-05-01 23:10:32 +0100

    adding two speeds

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
 var allEnemies = [];
 var player;
-var speed_Random = 1;
+var speed_Random = 1,speed_Random_1 = 1,speed_Random_2 = 1,speed_Random_3 = 1;
 var random_location;
 var pos_enemy = [];
 var pos_person = [];
-var addingX;
+var addingX = 1;
+var addingX_1 = 1;
+var addingX_2 = 1;
+var counter_1 = 1;
+var counter_2 = 1;
+var width = 1;
+var height = 1;
+var enemyCurrentPos = [];
+var enemyLastPos = 1;
 
 class Enemy {
     constructor() {
-        
-        pos_enemy = [[1] ,[61,141,221]];
-        // Variables applied to each of our instances go here,
-        
+        this.width = 80;
+        this.height = 80;
+        addingX = Math.floor(Math.random()*500);
+        pos_enemy = [[addingX] ,[61,141,221]];
+		// Variables applied to each of our instances go here,
+		console.log(pos_enemy);
+        console.log(addingX);
         random_location = Math.floor(Math.random() * 3);
-        this.x = pos_enemy[0][0];
+        this.x = pos_enemy[0][addingX];
         this.y = pos_enemy[1][random_location];
         // we've provided one for you to get started
         // The image/sprite for our enemies, this uses
@@ -23,18 +34,63 @@ class Enemy {
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
     update(dt) {
-        console.log(dt);
-        if(speed_Random < 700) {
-        speed_Random += 2;
-        }
-        if(speed_Random >= 700) {
-            speed_Random = 4;
-        }
+        //console.log(dt);
         speed_Random * dt;
-		speed_Random = Math.floor(speed_Random);
-		console.log(speed_Random);
-        this.x = speed_Random;
-         
+        addingX_1 * dt;
+        addingX_2 * dt;	
+
+		if(speed_Random <= 500) {
+			
+			//this.x = speed_Random;		
+        }
+        if(speed_Random >= 500) {
+            speed_Random = -100;
+        }
+        speed_Random += 1;
+        
+
+        if(counter_1 == 1){
+            counter_1++;
+            if(addingX_1 <= 500) {
+                if(this.y == 61) {
+                    this.x = addingX_1;
+                }
+                if(this.y == 141) {
+                    this.x = addingX_1;
+                }
+                if(this.y == 221) {
+                    this.x = addingX_1;
+                }
+            //    this.x = addingX_1;
+            }
+            if(addingX_1 >= 500) {
+                addingX_1 = -100;
+
+            }
+            addingX_1 += 1;
+        }else if(counter_1 == 2){
+            counter_1--;
+            if(addingX_1 <= 500) {
+                if(this.y == 61) {
+                    this.x = speed_Random;
+                }
+                if(this.y == 141) {
+                    this.x = speed_Random;
+                }
+                if(this.y == 221) {
+                    this.x = speed_Random;
+                }
+                //this.x = speed_Random;
+            }
+            if(addingX_1 >= 500) {
+                addingX_1 = -100;
+            }
+            addingX_1 += 2;
+        }
+
+        
+
+		//console.log(speed_Random); 
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
@@ -49,8 +105,9 @@ class Enemy {
 
 class Person {
     constructor() {
-        this.x = 101;
-        this.y = 404;
+		pos_person = [[101],[404]];
+        this.x = pos_person[0];
+        this.y = pos_person[1];
         // Variables applied to each of our instances go here,
         
         this.sprite = 'images/char-boy.png';
@@ -88,16 +145,19 @@ player = new Person();
 console.log(player);
  
 
-enemy = new Enemy();
-allEnemies.push(enemy);
-    
+newFunction();
+newFunction();
+newFunction();
+newFunction();
+
+
 
 
  
 //allEnemies.push(enemy);
 console.log(allEnemies);
 
-
+console.log(pos_enemy[1][0]);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -110,3 +170,8 @@ document.addEventListener('keyup', function(e) {
     };
     player.handleInput(allowedKeys[e.keyCode]);
 });
+function newFunction() {
+	enemy = new Enemy();
+	allEnemies.push(enemy);
+}
+
