7803ffc2d748cc6b6af3435cd6c06bda1afd5d6e
Paul Standley <paulstandley72@outlook.com>
2018-05-03 18:48:33 +0100

    start random speeds

--- js/app.js
+++ js/app.js
@@ -13,19 +13,42 @@ var width = 1;
 var height = 1;
 var enemyCurrentPos = [];
 var enemyLastPos = 1;
+var tileArray;
+var tileCount_x = 0;
+var tileCount_y = 0;
 
 class Enemy {
     constructor() {
         this.width = 80;
-        this.height = 80;
+        this.height = 70;
         addingX = Math.floor(Math.random()*500);
         pos_enemy = [[addingX] ,[61,141,221]];
 		// Variables applied to each of our instances go here,
 		console.log(pos_enemy);
         console.log(addingX);
-        random_location = Math.floor(Math.random() * 3);
+        console.log(`counter_2 ${counter_2}`);
+		if(counter_2 == 1) {
+            this.y = 61;
+            console.log(`counter_2 ${counter_2} and y ${this.y}`);
+        }
+		if(counter_2 == 2) {
+            this.y = 141;
+            console.log(`counter_2 ${counter_2} and y ${this.y}`);
+		}
+		if(counter_2 == 3) {
+            this.y = 221;
+            console.log(`counter_2 ${counter_2} and y ${this.y}`);
+		}
+		if(counter_2 == 4) {
+			counter_2 = 1;
+			random_location = Math.floor(Math.random() * 3);
+            this.y = pos_enemy[1][random_location];
+            console.log(`counter_2 ${counter_2} and y ${this.y}`);
+		}
+        counter_2++;
+    
         this.x = pos_enemy[0][addingX];
-        this.y = pos_enemy[1][random_location];
+        console.log(`counter_2 ${counter_2} and y ${this.y} and x ${this.x}`);    
         // we've provided one for you to get started
         // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
@@ -33,76 +56,94 @@ class Enemy {
     }
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
-    update(dt) {
-        //console.log(dt);
+    update(dt, tileArray) {
+		//console.log(this.tileArray);
+		//console.log(dt);
+		
+		
+
         speed_Random * dt;
         addingX_1 * dt;
         addingX_2 * dt;	
 
-		if(speed_Random <= 500) {
+		if(speed_Random <= 600) {
 			
 			//this.x = speed_Random;		
         }
-        if(speed_Random >= 500) {
+        if(speed_Random >= 600) {
             speed_Random = -100;
         }
         speed_Random += 1;
-        
+		
+// speed
+		if(speed_Random_2 <= 600) {
+			
+			//this.x = speed_Random;		
+        }
+        if(speed_Random_2 >= 600) {
+            speed_Random_2 = -100;
+        }
+        speed_Random_2 += 0.75;
 
         if(counter_1 == 1){
             counter_1++;
-            if(addingX_1 <= 500) {
+            if(speed_Random_2 <= 600 || speed_Random_2 >= -100) {
                 if(this.y == 61) {
+                    
                     this.x = addingX_1;
+                    
                 }
                 if(this.y == 141) {
-                    this.x = addingX_1;
+                    this.x = speed_Random;
                 }
                 if(this.y == 221) {
-                    this.x = addingX_1;
+                
+                    this.x = speed_Random_2;
+                
                 }
             //    this.x = addingX_1;
             }
-            if(addingX_1 >= 500) {
-                addingX_1 = -100;
+            if(addingX_1 >= 600 || addingX_1 <= -100) {
+                //addingX_1 = -100;
                 if(this.y  == 61) {
-                    this.y = 141;    
+                    
                 }
                 if(this.y == 141) {
-                    this.y = 221;
+            
 				}
 				if(this.y == 221) {
-					this.y = 61;
+					
 				}
             }
-            addingX_1 += 1;
+			addingX_1 += 0.5;
+			
         }else if(counter_1 == 2){
             counter_1--;
-            if(addingX_1 <= 500) {
+            if(addingX_1 <= 600 || addingX_1 >= -100) {
                 if(this.y == 61) {
-                    this.x = speed_Random;
+                    this.x = addingX_1;
                 }
                 if(this.y == 141) {
                     this.x = speed_Random;
                 }
                 if(this.y == 221) {
-                    this.x = speed_Random;
+                    this.x = speed_Random_2;
                 }
                 //this.x = speed_Random;
             }
-            if(addingX_1 >= 500) {
+            if(addingX_1 >= 600) {
 				addingX_1 = -100;
 				if(this.y  == 61) {
-                    this.y = 141;    
+              
                 }
                 if(this.y == 141) {
-                    this.y = 221;
+                    
 				}
 				if(this.y == 221) {
-					this.y = 61;
+					
 				}
             }
-            addingX_1 += 2;
+            addingX_1 += 0.5;
         }
 
         
@@ -122,19 +163,24 @@ class Enemy {
 
 class Person {
     constructor() {
-		pos_person = [[101],[404]];
+		pos_person = [[404],[404]];
         this.x = pos_person[0];
         this.y = pos_person[1];
         // Variables applied to each of our instances go here,
-        
+        this.personPosArray = [
+			[1,101,202,303,404],[1,101,202,303,404]
+		];
+		
         this.sprite = 'images/char-boy.png';
     }
     // Update, required method for game
     // Parameter: dt, a time delta between ticks
-    update(dt) {
+    update(dt, tileArray) {
 	
 		speed_Random_3 * dt;
-    
+	
+		
+
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
@@ -163,18 +209,18 @@ player = new Person();
 console.log(player);
  
 
-newFunction();
-newFunction();
-newFunction();
-newFunction();
+MakeEnemiesFunction();
+MakeEnemiesFunction();
+MakeEnemiesFunction();
+MakeEnemiesFunction();
 
 
 
 
  
 //allEnemies.push(enemy);
-console.log(allEnemies);
-
+console.log(...allEnemies);
+console.log();
 console.log(pos_enemy[1][0]);
 
 // This listens for key presses and sends the keys to your
@@ -188,7 +234,7 @@ document.addEventListener('keyup', function(e) {
     };
     player.handleInput(allowedKeys[e.keyCode]);
 });
-function newFunction() {
+function MakeEnemiesFunction() {
 	enemy = new Enemy();
 	allEnemies.push(enemy);
 }
