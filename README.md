b836af871d5e23123f79799c0f9534ec26909df0
Paul Standley <paulstandley72@outlook.com>
2018-05-15 18:55:29 +0100

    validation changes md images

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
-frontend-nanodegree-arcade-game
-===============================
+# Game project 4
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+no dependencys
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+[my github game link]('https://github.com/paulstandley/Project-Udacity-4')
 
-Project Brake down
-==================
+## How to load the game
 
-## what needs doing 
+open file click on index.html to load the game
 
-### ~~The game functions correctly and runs error free~~
+![Start]('http://res.cloudinary.com/pieol2/image/upload/v1526402515/gameStart.png')
+
+## How to play
+
+### keybord
+
+move player by keybord arrow keys
+
+doge the bugs they remove your lifefore if your lifefore reaches 0 you die :(
+
+![lostGame]('http://res.cloudinary.com/pieol2/image/upload/v1526402772/lost.png')
+
+get into the water to score you need a score off scores to win :)
+
+![winGame]('http://res.cloudinary.com/pieol2/image/upload/v1526402944/win.png')
+
+## what needs doing
+
+~~The game functions correctly and runs error free~~
 
 ~~Player can not move off screen~~
 
@@ -22,18 +38,6 @@ Project Brake down
 
 ~~Something happens when player wins~~
 
-## ~~Basic Functionality~~
+~~Basic Functionality~~
 
-In this game you have a Player and Enemies (Bugs). The goal of the player is to reach the water, without colliding into any one of the enemies. The player can move left, right, up and down. The enemies move in varying speeds on the paved block portion of the scene. Once a the player collides with an enemy, the game is reset and the player moves back to the start square. Once the player reaches the water the game is won.
-
-
-## Additional Functionality
-
-In addition to the basic functionality, you can add more cool functionality to your game. For example, here are some additional features that you can add:
-
-Player selection: allow the user to select the image for the player character before starting the game. You can use the different character images provided in the images folder (we’ll get to that below).
-
-Score: you can implement a score for the game. For example, the score can increase each time the player reaches the water, and it can be reset to 0 when collision occurs (or it can be reduced).
-
-Collectibles: you can add gems to the game, allowing the player to collect them to make the game more interesting.
-Anything else you like!
+~~In this game you have a Player and Enemies (Bugs). The goal of the player is to reach the water, without colliding into any one of the enemies. The player can move left, right, up and down. The enemies move in varying speeds on the paved block portion of the scene. Once a the player collides with an enemy, the game is reset and the player moves back to the start square. Once the player reaches the water the game is won.~~
--- js/app.js
+++ js/app.js
@@ -15,14 +15,14 @@ var playerScore = 0,playersLives = 100;
 var h3score = document.getElementById("displayScore");
 var h3lives = document.getElementById("displayLives");
 var fullbodydiv = document.getElementById("fullBody");
+
 class Enemy {
     constructor() {
         this.width = 80;
         this.height = 70;
         addingX = Math.floor(Math.random()*500);
         pos_enemy = [[addingX] ,[61,141,221]];
-		// Variables applied to each of our instances go here,
-		
+		// Variables applied to each of our instances go here,		
 		if(counter_2 == 1) {
             this.y = 61;
         }
@@ -43,24 +43,14 @@ class Enemy {
         counter_2++;
     
         this.x = pos_enemy[0][addingX];
-        //console.log(`counter_2 ${counter_2} and y ${this.y} and x ${this.x}`);    
-        // we've provided one for you to get started
-        // The image/sprite for our enemies, this uses
-        // a helper we've provided to easily load images
+        
         this.sprite = 'images/enemy-bug.png';
     }
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
+
     update(dt, tileArray) {
-	
-        speed_Random * dt;
-        speed_Random_2 * dt;  
-        addingX_1 * dt;
-        addingX_2 * dt;	
-        addingX_3 * dt;
-        addingX_4 * dt;
-        addingX_5 * dt;
-        addingX_6 * dt;
+        this.setDTMethod(dt);
 
         this.displayMethod();
 
@@ -69,81 +59,97 @@ class Enemy {
         this.displayOnPage();
     }
 
+    setDTMethod(dt) {
+        speed_Random * dt;
+        speed_Random_2 * dt;
+        addingX_1 * dt;
+        addingX_2 * dt;
+        addingX_3 * dt;
+        addingX_4 * dt;
+        addingX_5 * dt;
+        addingX_6 * dt;
+    }
+
     displayOnPage() {
         h3lives.innerText = playersLives;
         h3score.innerText = playerScore;
         if(playersLives <= 0) {
             fullbodydiv.innerHTML = `<h2 id="h2lost">You got dead</h2>`; 
             player.sprite = "images/Heart.png";
-            setTimeout(reLoadGame, 3000);
+            allEnemies.forEach(function(val){
+                val.sprite = "images/Heart.png";
+            });
+            setTimeout(reLoadGame, 6000);
         }
-        if(playerScore >= 10) {
+        if(playerScore >= 20) {
             fullbodydiv.innerHTML = `<h2 id="h2win">Well Done</h2>`;
             player.sprite = "images/Star.png";
-            setTimeout(reLoadGame, 3000);
+            allEnemies.forEach(function(val){
+                val.sprite = "images/Star.png";
+            });
+            setTimeout(reLoadGame, 6000);
         }
     }
 
     displayMethod() {
+        /* make enemys seam  randon  */
         if(counter_3 == 1) {
-        if (addingX_1 <= 500 || addingX_1 >= -100) {
-            this.x = addingX_1;
-            if (addingX_1 >= 500) {
-                addingX_1 = -100;
+            if (addingX_1 <= 500 || addingX_1 >= -100) {
+                this.x = addingX_1;
+                if (addingX_1 >= 500) {
+                    addingX_1 = -100;
             }
             addingX_1 += 5;
         }
-    }else if(counter_3 == 2) {
-        if (addingX_2 <= 600 || addingX_2 >= -300) {
-            this.x = addingX_2;
-            if (addingX_2 >= 600) {
-                addingX_2 = -300;
+        }else if(counter_3 == 2) {
+            if (addingX_2 <= 600 || addingX_2 >= -300) {
+                this.x = addingX_2;
+                if (addingX_2 >= 600) {
+                    addingX_2 = -300;
+                }
+                addingX_2 += 4.5;
             }
-            addingX_2 += 4.5;
-        }
-    }else if(counter_3 == 3) {
-        if (addingX_3 <= 700 || addingX_3 >= -400) {
-            this.x = addingX_3;
-            if (addingX_3 >= 700) {
-                addingX_3 = -400;
+        }else if(counter_3 == 3) {
+            if (addingX_3 <= 700 || addingX_3 >= -400) {
+                this.x = addingX_3;
+                if (addingX_3 >= 700) {
+                    addingX_3 = -400;
+                }
+                addingX_3 += 4;
             }
-            addingX_3 += 4;
-        }
-    }else if(counter_3 == 4) {
-        if (addingX_4 <= 950 || addingX_4 >= -150) {
-            this.x = addingX_4;
-            if (addingX_4 >= 950) {
-                addingX_4 = -150;
+        }else if(counter_3 == 4) {
+            if (addingX_4 <= 950 || addingX_4 >= -150) {
+                this.x = addingX_4;
+                if (addingX_4 >= 950) {
+                    addingX_4 = -150;
+                }
+                addingX_4 += 5.5;
             }
-            addingX_4 += 5.5;
-        }
-    }else if(counter_3 == 5) {
-        if (addingX_5 <= 850 || addingX_5 >= -300) {
-            this.x = addingX_5;
-            if (addingX_5 >= 850) {
-                addingX_5 = -300;
+        }else if(counter_3 == 5) {
+            if (addingX_5 <= 850 || addingX_5 >= -300) {
+                this.x = addingX_5;
+                if (addingX_5 >= 850) {
+                    addingX_5 = -300;
+                }
+                addingX_5 += 3.5;
             }
-            addingX_5 += 3.5;
-        }
-    }else if(counter_3 == 6) {
-        if (addingX_6 <= 800 || addingX_6 >= -200) {
-            this.x = addingX_6;
-            if (addingX_6 >= 800) {
-                addingX_6 = -200;
+        }else if(counter_3 == 6) {
+            if (addingX_6 <= 800 || addingX_6 >= -200) {
+                this.x = addingX_6;
+                if (addingX_6 >= 800) {
+                    addingX_6 = -200;
+                }
+                addingX_6 += 3;
             }
-            addingX_6 += 3;
         }
-    }
-    counter_3++;
-    if(counter_3 > 6) {
-        counter_3 = 1;
-    }
+        counter_3++;
+        if(counter_3 > 6) {
+            counter_3 = 1;
+        }
     }
 
     enemyCollMethod() {
-
         for(let crash in allEnemies) {
-            
             if(player.y == 80  && allEnemies[crash].y == 61) {
                 if(allEnemies[crash].x <= player.x + 40 && allEnemies[crash].x >= player.x - 40) {
                     delta_v_x = 202;
@@ -165,7 +171,6 @@ class Enemy {
             }
         }
     }
-
     // Draw the enemy on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -183,93 +188,85 @@ class Person {
         this.personPosArray = [
 			[1,101,202,303,404],[1,101,202,303,404]
 		];
-		
         this.sprite = 'images/char-boy.png';
     }
     // Update, required method for game
     // Parameter: dt, a time delta between ticks
     update(dt) {
-        
         this.x = delta_v_x;
         this.y = delta_v_y;
-        
     }
 
     handleInput(evt) {    
-/* up down left and right controls and border detection  */
-       
+/* up down left and right controls and border detection  */      
         this.playerMoveMethod(evt); 
 /*    detect enemy   */
         this.personCollMethod();
     }
+
     playerMoveMethod(evt) {
-        if (delta_v_x < 0) {
+        if(delta_v_x < 0) {
             delta_v_x = 0;
             this.x = delta_v_x;
         }
-        else if (delta_v_x >= 0 && delta_v_x <= 404) {
-            if (evt == "left") {
-                if (delta_v_x == 0) {
+        else if(delta_v_x >= 0 && delta_v_x <= 404) {
+            if(evt == "left") {
+                if(delta_v_x === 0) {
                     // dont do anything
                 }
-                else {
+                else{
                     this.x = delta_v_x += -101;
                 }
             }
-            if (evt == "right") {
-                if (delta_v_x == 404) {
+            if(evt == "right") {
+                if(delta_v_x == 404) {
                     // dont do anything
                 }
-                else {
+                else{
                     this.x = delta_v_x += 101;
                 }
             }
             // set delta v x back to max             
         }
-        else if (delta_v_x > 404) {
+        else if(delta_v_x > 404) {
             delta_v_x = 404;
             this.x = delta_v_x;
         }
-        /*    up and down controls and border detection  */        
-        
-        if (delta_v_y >= 0 && delta_v_y <= 400) {
+        /*    up and down controls and border detection  */               
+        if(delta_v_y >= 0 && delta_v_y <= 400) {
             if (evt == "up") {
-                if (delta_v_y == 80) {
+                if(delta_v_y == 80) {
                     delta_v_y = 400;
                     playerScore++;
-                }
-                else {
+                }else{
                     this.y = delta_v_y += -80;
                 }
             }
-            if (evt == "down") {
-                if (delta_v_y == 400) {
+            if(evt == "down") {
+                if(delta_v_y == 400) {
                     // dont do anything 
-                }
-                else {
+                }else{
                     this.y = delta_v_y += 80;
                 }
             }
             // set delta v y back to max             
-        }
-        else if (delta_v_y > 400) {
+        }else if(delta_v_y > 400) {
             delta_v_y = 400;
             this.y = delta_v_y;
         }
     }
 
     personCollMethod() {
-        for (let i = 0; i < allEnemies.length; i++) {       
-            if (allEnemies[i].x >= (this.x + this.width) || (allEnemies[i].x + allEnemies[i].width) <= this.x || allEnemies[i].y >= (this.y + this.height) || (allEnemies[i].y + allEnemies[i].height) <= this.y) {
+        for(let i = 0; i < allEnemies.length; i++) {       
+            if(allEnemies[i].x >= (this.x + this.width) || (allEnemies[i].x + allEnemies[i].width) <= this.x || allEnemies[i].y >= (this.y + this.height) || (allEnemies[i].y + allEnemies[i].height) <= this.y) {
             }
-            else {
+            else{
                 delta_v_x = 202;
                 delta_v_y = 400;
                 playersLives--;
             }
         }
     }
-
     // Draw the enemy on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -283,15 +280,12 @@ class Person {
 // Place the player object in a variable called player
 //var enemy = new Enemy();
 player = new Person(); 
-
 MakeEnemiesFunction();
 MakeEnemiesFunction();
 MakeEnemiesFunction();
 MakeEnemiesFunction();
 MakeEnemiesFunction();
 MakeEnemiesFunction();
-//allEnemies.push(enemy);
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(evt) {
@@ -312,4 +306,3 @@ function MakeEnemiesFunction() {
 function reLoadGame() {
     window.location.reload(false);
 }
-
